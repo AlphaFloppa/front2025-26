117c8e87cad9e025b0e7fc742af204d47807373a
Leukhin Maksim <107624364+NihaoHaos@users.noreply.github.com>
2025-09-25 12:23:18 +0300

    a

--- my-app/src/Original/Toolbar/Toolbar.tsx
+++ my-app/src/Original/Toolbar/Toolbar.tsx
@@ -1,5 +1,7 @@
 import { Button } from "../../Common/Button/Button"
 import type {Image} from "../../Store/Model/slideContent"
+import style as toolbarStyle from "../Toolbar/Toolbar.module.css"; 
+import style as btnStyle from "../Button/Button.module.css";
 
 export function Toolbar(
     props: {
@@ -18,12 +20,12 @@ export function Toolbar(
     }
 ) {
     return
-        <div className = "toolbar">
-            <section className="add-object">
-                <Button class = "text" clickHandler={() => props.eventHandlers.addText} icon={props.icons.addText}/>
+        <div className = {toolbarStyle.toolbar}>
+            <section className = {toolbarStyle.section}>
+                <Button class =  clickHandler={() => props.eventHandlers.addText} icon={props.icons.addText}/>
                 <Button class = "image" clickHandler={() => props.eventHandlers.addImage} icon={props.icons.addImg}/>
             </section>
-            <section className = "text-props-editor">
+            <section className = {toolbarStyle.section}>
                 <Button class = "font-family" clickHandler={() => props.eventHandlers.editFontFamily} icon={props.icons.addText}/>
                 <Button class = "font-size" clickHandler={() => props.eventHandlers.editFontSize} icon={props.icons.addText}/>
             </section>
